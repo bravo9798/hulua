f0aa0d12f0e987e225a19a4490315673d330ee83
bravo9798 <bhuvneshsharma9798@gmail.com>
2021-09-15 16:22:26 +0530

    Adding request and fetching movies data from api

--- utils/requests.js
+++ utils/requests.js
@@ -7,37 +7,44 @@ export default {
     },
     fetchTopRated:{
         title:"Top Rated",
-        url:`/movie/top_rated?api_key=${API_KEY}&`
+        url:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     },
     fetchActionMovies:{
         title:"Action",
-        url:`/movie/tok`
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
     },
 
     fetchComedyMovies:{
         title:"Comedy",
-
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
     },
     fetchHorrorMovies:{
         title:"Horror",
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
     },
     fetchRomanceMovies:{
-        title:"Romance"
+        title:"Romance",
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     },
     fetchMystery:{
-        title:"Mystery"
+        title:"Mystery",
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=9648`,
     },
     fetchSciFi:{
-        title:"Scifi"
+        title:"Scifi",
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=878`,
     },
     fetchWestern:{
-        title:"Western"
+        title:"Western",
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=37`,
     },
     fetchAnimation:{
-        title:"Animation"
+        title:"Animation",
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=16`,
     },
     fetchTV:{
         title:"TV Movie",
+        url:`/discover/movie?api_key=${API_KEY}&with_genres=10770`,
     }
 
 
